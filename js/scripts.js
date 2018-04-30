f64309b393f27db8dbb253d25ee1af38db21f002
Scott Dunham <dunham.scott@gmail.com>
2018-04-29 21:55:11 -0700

    Update archive year/month select functionality to better support only selecting a year

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,34 @@
 		// When the archive year select field changes, update the month select field
 		// with months in that year with at least one post
 		$('select#postYearSelect').on('change', function(e){
-			// Get new year selection
 			var year = $(this).val();
 
+			updatePostsAndMonthsByYear(year);
+		});
+
+		// When the archive month select field changes, update the list of posts displayed
+		$('select#postMonthSelect').on('change', function(e, skipUpdate){
+			var year = $('select#postYearSelect').val();
+			var month = $(this).val();
+
+			// If an actual month is selected, just update the list of posts based on the current year and month
+			if(month){
+				updatePostsByYearAndMonth(year, month);
+			}
+			// If not told to skip an update, update the posts and month select based on the currently selected year
+			else if(!skipUpdate){
+				updatePostsAndMonthsByYear(year);
+			}
+		});
+
+		// Takes a year and gets a list of posts and months with posts from that year
+		// Updates list of posts and month select options with results
+		// If no year is passed, the default search text is shown instead
+		function updatePostsAndMonthsByYear(year){
 			// Clear month select options
 			$('select#postMonthSelect option').remove();
 
-			// If there isn't an actual value selected, clear the posts and return
+			// If there isn't an actual year selected, clear the posts and return
 			if(!year){
 				updatePostArchiveMarkup('<em>'+scriptParams.archiveDefaultText+'</em>');
 				return;
@@ -40,24 +61,17 @@
 				addOptionsToMonthSelect(res.months);
 				updatePostArchiveMarkup( res.postsMarkup );
 			});
-		});
+		}
 
-		// When the archive month select field changes, update the list of posts displayed
-		$('select#postMonthSelect').on('change', function(e){
-			// Get new month selection
-			var month = $(this).val();
-
-			// If there isn't an actual value selected, clear the posts and return
+		// Takes a year and month, and updates post list with post from that year and month
+		function updatePostsByYearAndMonth(year, month){
+			// If there isn't an actual month selected, return
 			if(!month){
-				updatePostArchiveMarkup('<em>'+scriptParams.archiveDefaultText+'</em>');
 				return;
 			}
 
 			// Remove previous posts markup
 			updatePostArchiveMarkup('');
-
-			// Get the selected year
-			var year = $('select#postYearSelect').val();
 
 			// Show loading animation
 			$('#postArchiveLoading').removeClass('cb-hidden');
@@ -78,9 +92,7 @@
 
 				updatePostArchiveMarkup( postsMarkup );
 			});
-
-			''
-		});
+		}
 
 		// Update the post month select based on the array of months provided
 		function addOptionsToMonthSelect(arrMonths){
@@ -93,7 +105,9 @@
 				select.append(option);
 			});
 
-			select.trigger('change');
+			// Trigger a change on the update month select, but also pass an
+			// extra parameter to the change event to avoid updating any markup
+			select.trigger('change', [true]);
 		}
 
 		// Updates the archive page's post markup with the provided string
